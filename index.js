6a9e961a4938e98eb41079c2a69921bd182b6c20
Taylor Williams <taylor@thewilliams.net>
2019-02-03 03:11:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+let randomNumber = Math.random * 20
+let randomInteger = Math.floor(randomNumber)
